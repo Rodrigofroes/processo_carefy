177a64ea56ca59fc2ea2966b955d47d8a066cf1c
RodrigoFroes <craftland262@gmail.com>
2025-02-10 20:51:18 -0300

    Feat: Ajustes

--- repositories/logRepository.js
+++ repositories/logRepository.js
@@ -3,15 +3,24 @@ import { openDb } from "../db/database.js";
 const db = openDb();
 
 export default class LogRepository {
-    async adicionarLog(log){
+    async adicionarLog(log) {
         const banco = await db;
         const cadastrar = await banco.run('INSERT INTO logs (metodo, url, status, filme_id) VALUES (?, ?, ?, ?)', [log.metodo, log.url, log.status, log.filme_id]);
         return cadastrar;
     }
 
-    async listarLogs(){
+    async listarLogs(page = 1, limite = 5) {
         const banco = await db;
-        const listar = await banco.all('SELECT * FROM logs');
-        return listar;
+        const offset = (page - 1) * limite;
+
+        const listar = await banco.all('SELECT * FROM logs LIMIT ? OFFSET ?', [parseInt(limite), parseInt(offset)]);
+        const { total } = await banco.get('SELECT COUNT(*) AS total FROM logs');
+
+        return {
+            listar,
+            total,
+            totalPages: Math.ceil(total / limite),
+            currentPage: page
+        };
     }
 }
--- routes/logRoute.js
+++ routes/logRoute.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import LogsController from '../controllers/logsController.js';
+import AuthLogin from '../middleware/AuthLogin.js';
 
 const router = express.Router();
 const logsController = new LogsController();
+const auth = new AuthLogin();
 
-router.get('/logs', (req, res) => {
+router.get('/logs', auth.validar, (req, res) => {
     // #swagger.tags = ['Logs']
     // #swagger.summary = 'Listar logs'
     logsController.listarLogs(req, res);
